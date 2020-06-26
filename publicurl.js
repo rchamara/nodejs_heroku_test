d406935331f75f8a9f6b0d37ab425b1e67dfcc52
Chamara Manchanayaka <chamara19911218@gmail.com>
2020-06-26 14:09:32 +0530

    fixed error

--- publicurl.js
+++ publicurl.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Gator app listening on port 3000!'));
